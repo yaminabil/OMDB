7d0fd548782e85213a93ba7a130973ec8ef2682b
Bilal Younes <bilal94.younes@gmail.com>
2022-04-09 10:08:35 -0400

    Cathy note commited

--- src/App.js
+++ src/App.js
@@ -1,36 +1,32 @@
 import MovieDisplay from "./components/MovieDisplay";
-import Form from  "./components/Form";
-import Header from "./components/Header"
-import Footer  from "./components/Footer";
+import Form from "./components/Form";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { useState } from "react";
 
-import {useState} from "react";
+export default function App() {
+  const apiKey = "97619273";
+  const [movie, setMovie] = useState(null);
 
-
-export default function App (){
-    const apiKey = "97619273";
-    const [movie,setMovie] = useState(null);
-
-    //function that get the data 
-    const getMovie = async (searchTerm)=> {
-
-        try {
-            const response = await fetch (`http://www.omdbapi.com/?apikey=${apiKey}&t=${searchTerm}`);
-            const data = await response.json();
-            setMovie(data);
-
-        } catch (error) {
-            console.log(error);
-        }
-
+  //function that get the data
+  const getMovie = async (searchTerm) => {
+    try {
+      const response = await fetch(
+        `https://www.omdbapi.com/?apikey=${apiKey}&t=${searchTerm}`
+      );
+      const data = await response.json();
+      setMovie(data);
+    } catch (error) {
+      console.log(error);
     }
+  };
 
-
-    return (
-        <div className = "App">
-            <Header />
-            <Form moviesearch = {getMovie} />
-            <MovieDisplay movie = {movie} />
-            <Footer />
-            </div>
-    )
+  return (
+    <div className="App">
+      <Header />
+      <Form moviesearch={getMovie} />
+      <MovieDisplay movie={movie} />
+      <Footer />
+    </div>
+  );
 }
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,30 +1,30 @@
 import { useState } from "react";
 
-export default function Form(props){
-    const [formData, setFormData] = useState({
-        searchTerm: ""
-    })
-    
-    const handleChange = (event) => {
-        setFormData({...formData, [event.target.name]: event.target.value })
-    }
+export default function Form(props) {
+  const [formData, setFormData] = useState({
+    searchTerm: "",
+  });
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        props.moviesearch(formData.searchTerm)
-    }
+  const handleChange = (event) => {
+    setFormData({ ...formData, [event.target.name]: event.target.value }); //(...) for all form data
+  };
 
-    return (
-        <div>
-            <form onSubmit={handleSubmit}>
-                <input 
-                type="text" 
-                name="searchTerm"
-                onChange={handleChange}
-                value={formData.searchTerm}
-                /> 
-                <input type="submit" value="ACTION!"/>
-            </form>
-        </div>
-    )
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    props.moviesearch(formData.searchTerm);
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="searchTerm"
+          onChange={handleChange}
+          value={formData.searchTerm}
+        />
+        <input type="submit" value="ACTION!" />
+      </form>
+    </div>
+  );
 }
